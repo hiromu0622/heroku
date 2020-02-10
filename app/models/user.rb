d26d9aea8b8e4b363ff97528e8eeb5f491e85857
hiromu takahashi <htakahashi262@gmail.com>
2020-02-10 09:52:46 +0000

    Rails-05-task

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   #課題　パスワードの文字数制限を追加する 8~32
   validates :password, length: { in: 1..32 }
   #パスワードをアルファベット、数字の混合のみ可能にしてください
-  VALID_PASSWORD_REGEX = /\A[a-zA-Z\d]+\z/
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/
    validates :password,  format: { with:VALID_PASSWORD_REGEX}
   #メールアドレスの正規表現を追加してください ~ @ ~ . ~
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
